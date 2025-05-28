07789dd169e817a7b19102cea20289837bd24074
mktveni33 <medikondateja9@gmail.com>
2025-05-28 10:43:31 +0530

    Fix anatomy image import and move image inside src/assets

--- src/Components/DashboardMainContent/AnatomySection/AnatomySection.js
+++ src/Components/DashboardMainContent/AnatomySection/AnatomySection.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './AnatomySection.css';
+import anatomyImage from '../../../Assets/anotomyImage.jpg';
+
+
 
 function AnatomySection() {
   return (
     <div className="anatomy-section">
       <div className="anatomy-image-wrapper">
-        <img src="./anatomy.jpg" alt="Anatomy" className="anatomy-image" />
-
+            <img src={anatomyImage} alt="Anatomy" className="anatomy-image" /> 
     
         <div className="indicator heart">Healthy Heart</div>
         <div className="indicator lungs">Lungs</div>
